169a392e8242d914a618fd00492234cf8a4ec056
PRATHAMMOHANTY <pratham.mohanty.7@gmail.com>
2025-04-25 10:32:14 +0000

    dotnetpr

---
+++ dotnetapp/Models/SavingsPlan.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace dotnetapp.Models
+{
+    public class SavingsPlan
+    {
+        [Key]
+        public int SavingsPlanId { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(1000, 10000000)]
+        public decimal GoalAmount { get; set; }
+
+        [Required]
+        [Range(1,50)]
+        public int TimeFrame { get; set; }
+
+        [Required]
+        public string RiskLevel { get; set; }
+        
+        [Required]
+        [MaxLength(500)]
+        public string Description { get; set; }
+        [Required]
+        public string Status { get; set; }
+
+
+    }
+}
