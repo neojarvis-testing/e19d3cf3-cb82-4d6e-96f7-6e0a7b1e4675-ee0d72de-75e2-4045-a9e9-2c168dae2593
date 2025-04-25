8ca4a95245f76dbb0fa1d0f4e0a11599c26761ad
RKarthik124562 <karthik33r@gmail.com>
2025-04-25 11:30:10 +0000

    angular components day_1

---
+++ angularapp/src/app/components/managernav/managernav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-managernav',
+  templateUrl: './managernav.component.html',
+  styleUrls: ['./managernav.component.css']
+})
+export class ManagernavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ angularapp/src/app/components/userviewsavingsplan/userviewsavingsplan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-userviewsavingsplan',
+  templateUrl: './userviewsavingsplan.component.html',
+  styleUrls: ['./userviewsavingsplan.component.css']
+})
+export class UserviewsavingsplanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
