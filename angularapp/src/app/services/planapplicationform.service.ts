4a427873f14583da73ec75ee27566d4fcb2ffb8e
RKarthik124562 <karthik33r@gmail.com>
2025-04-28 05:52:58 +0000

    services angular day_3

--- angularapp/src/app/services/planapplicationform.service.ts
+++ angularapp/src/app/services/planapplicationform.service.ts
@@ -1,9 +1,57 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { PlanApplication } from '../models/planapplication.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlanapplicationformService {
 
-  constructor() { }
+  plan: PlanApplication = {
+    AppliedAmount: 0,
+    Status: '',
+    ApplicationDate: '',
+    Remarks: '',
+    ProofDocument: '',
+    UserId: 0,
+    SavingsPlanId: 0
+  }
+
+  constructor(private http: HttpClient) { }
+
+  public apiUrl: string = ''
+
+  public addPlanApplication(plan: PlanApplication): Observable<PlanApplication> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + localStorage.getItem('Token')
+      })
+    };
+    return this.http.post<PlanApplication>(`${this.apiUrl}/api/PlanApplication/`, plan, httpOptions);
+  }
+
+  deletePlanApplication(planId: number): Observable<void> {
+    console.log("delete plan in serv ", planId);
+    return this.http.delete<void>(`${this.apiUrl}/api/PlanApplication/${planId}`);
+  }
+
+  getAppliedPlans(userId: number): Observable<PlanApplication[]> {
+    return this.http.get<PlanApplication[]>(`${this.apiUrl}/api/PlanApplication/user/${userId}`);
+  }
+
+  getAllPlanApplications(): Observable<PlanApplication[]> {
+    return this.http.get<PlanApplication[]>(`${this.apiUrl}/api/PlanApplication`);
+  }
+
+  updatePlanApplication(planId: number, updatedData: PlanApplication): Observable<PlanApplication> {
+    return this.http.put<PlanApplication>(`${this.apiUrl}/api/PlanApplication/${planId}`, updatedData);
+  }
+
+  getPlanApplicationsBySavingsPlanIdAndUserId(savingsPlanId: number, userId: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/api/PlanApplication/savingsPlan/${savingsPlanId}/user/${userId}`);
+  }
+
+
 }
