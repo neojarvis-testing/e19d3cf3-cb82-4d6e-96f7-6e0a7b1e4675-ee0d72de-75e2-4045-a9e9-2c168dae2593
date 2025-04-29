5199b4f695b32f68c6a5f4a25a627ef162cff1ad
pendyalapavan <pendyalapavan0@gmail.com>
2025-04-29 11:21:22 +0000

    demooo

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ErrorComponent } from './components/error/error.component';
